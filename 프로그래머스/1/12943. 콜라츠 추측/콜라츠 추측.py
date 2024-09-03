9e7723e01eb4e2b59965f8733bd241e5998a2255
spatz <165136953+playdongsan@users.noreply.github.com>
2024-09-03 20:31:38 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.24 ms, Memory: 9.95 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,18 @@
+def solution(num):
+    count = 0
+    
+    if num == 1:
+        return 0
+    
+    while num != 1:
+        if num % 2 == 0:
+            num = num / 2
+            count += 1
+        elif num % 2 == 1:
+            num = num * 3 + 1
+            count += 1
+        if count >= 500:
+            return -1
+            break
+    
+    return count
