b2997ff642df052c0e715395b55acbc8c9d6505b
YUNCHANGHYUK <165136953+YUNCHANGHYUK@users.noreply.github.com>
2024-11-22 22:43:08 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 36 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -0,0 +1,9 @@
+S = input()
+positions = [-1] * 26
+
+for i, char in enumerate(S):
+    index = ord(char) - ord('a')
+    if positions[index] == -1:
+        positions[index] = i
+
+print(' '.join(map(str, positions)))
